18aa4a3d35c79413750fa1d1346f6c8ad52b69a4
Moogle <lizardscruff@users.noreply.github.com>
2018-11-20 21:39:12 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,13 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+	if (lockBoard) return;
+	if (this === firstCard) return;
+
 	this.classList.add('flip');
 
 	if (!hasFlippedCard) {
@@ -15,8 +19,6 @@ function flipCard() {
 	}
 
 	secondCard = this;
-	hasFlippedCard = false;
-
 	checkForMatch();
 }
 
@@ -29,15 +31,35 @@ function checkForMatch() {
 function disableCards() {
 	firstCard.removeEventListener('click', flipCard);
 	secondCard.removeEventListener('click', flipCard);
+
+	resetBoard();
 }
 
 function unflipCards() {
+	lockBoard = true;
+
 	setTimeout(() => {
 		firstCard.classList.remove('flip');
 		secondCard.classList.remove('flip');
+
+		resetBoard();
 	}, 1500);
 }
 
+function resetBoard() {
+	// this is a Destructuring Assignment
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+	[hasFlippedCard, lockBoard] = [false, false];
+	[firstCard, secondCard] = [null, null];
+}
 
+// this is an Immideiately Invoked Function Expression
+// https://developer.mozilla.org/en-US/docs/Glossary/IIFE
+(function shuffle() {
+	cards.forEach(card => {
+		let randomPos = Math.floor(Math.random() * 12);
+		card.style.order = randomPos;
+	});
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
